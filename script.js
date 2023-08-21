6399ab5f667b7426175376de7730b3a8f1e5def4
Wilder <w.cossio.r@gmail.com>
2023-08-20 20:06:58 -0400

    animation when press menu button & adding options

---
+++ script.js
@@ -0,0 +1,7 @@
+let container = document.querySelector('.container');
+let containerMenu = document.querySelector('.containerMenu');
+
+function rotate() {
+    containerMenu.style.transform = "rotate(-90deg)";
+    container.style.transform = "rotate(-15deg)";
+}

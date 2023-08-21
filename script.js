b840bf79ada1d9f09b19944858937846cc30ca70
Wilder <w.cossio.r@gmail.com>
2023-08-21 18:08:32 -0400

    refactoring nav by only adding a class through js

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
 let container = document.querySelector('.container');
 let containerMenu = document.querySelector('.containerMenu');
-let nav = document.querySelector('nav');
-
 
 function rotate() {
     container.classList.add('rotateMenu');
-    // container.style.transform = "rotate(-20deg)";
-    nav.style.left = "0px";
 }
 function rotateBack() {
     container.classList.remove('rotateMenu');
-    // container.style.transform = "rotate(0deg)";
-    nav.style.left = "-250px";
 }

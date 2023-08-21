f0e1698a3dd46d69e2d888ce4e14cee3bd0c8d77
Wilder <w.cossio.r@gmail.com>
2023-08-20 21:02:59 -0400

    adding animation rotateBack

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 let container = document.querySelector('.container');
 let containerMenu = document.querySelector('.containerMenu');
+let nav = document.querySelector('nav');
+
 
 function rotate() {
     containerMenu.style.transform = "rotate(-90deg)";
-    container.style.transform = "rotate(-15deg)";
+    container.style.transform = "rotate(-20deg)";
+    nav.style.left = "0px";
+}
+function rotateBack() {
+    containerMenu.style.transform = "rotate(0deg)";
+    container.style.transform = "rotate(0deg)";
+    nav.style.left = "-250px";
 }
